7e4b4292be59c07e8bfeaa20938f3d80d3b01e1e
woodchuckchoi <choiggoggi@gmail.com>
2020-10-03 23:33:35 +0900

    rudimentary test for some basic stuff that confused me in Python

--- c/structure/test.c
+++ c/structure/test.c
@@ -10,5 +10,13 @@ int main() {
 	test[0].a = 5;
 	test[0].b = 10;
 	printf("%d\n", test[0].a);
+
+	struct test *oi; // oi points to NULL, oi only holds storage big enough for the address of of a potential struct test variable.
+	/*
+	oi->a = 8;
+	oi->b = 16;
+	*/
+	printf("%p\n", oi); // shows nill value
+	
 	return 0;
 }
