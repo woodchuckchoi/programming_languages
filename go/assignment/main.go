89bf5696515700b1d069dd86b556b5fc5061d82d
woodchuckchoi <choiggoggi@gmail.com>
2020-12-05 19:20:53 +0900

    word counter done

--- go/assignment/main.go
+++ go/assignment/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
+
 type KeyValueStorage struct {
 	Keys      []string       `json:"keys"`
 	Values    map[string]int `json:"values"`
@@ -11,12 +18,26 @@ func (k *KeyValueStorage) AssignPartition(p string) error {
 	return nil
 }
 
+func (k *KeyValueStorage) Preprocess() error {
+	k.Partition = strings.ToLower(k.Partition)
+
+	regexp, err := regexp.Compile(`[^a-zA-Z0-9*]`)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	k.Partition = regexp.ReplaceAllString(k.Partition, " ")
+
+	return nil
+}
+
 func (k *KeyValueStorage) Process() error {
+	_ = k.Preprocess()
 	left, right := 0, 0
 	for right < len(k.Partition) {
 		if k.Partition[right] == ' ' {
 			if left < right {
-				k.Values[k.Partition[left:right]]++
+				k.Values[strings.ToLower(k.Partition[left:right])]++
 
 				left = right + 1
 				for left < len(k.Partition) && k.Partition[left] == ' ' {
@@ -36,4 +57,22 @@ func (k *KeyValueStorage) Process() error {
 
 func (k *KeyValueStorage) GetValue(key string) int {
 	return k.Values[key]
+}
+
+func InitKeyValueStorage(p string) *KeyValueStorage {
+	k := KeyValueStorage{
+		Keys:      []string{},
+		Values:    map[string]int{},
+		Partition: p,
+	}
+
+	return &k
+}
+
+func main() {
+	k := InitKeyValueStorage("This is the example sentence. This sentence may contain unexpected symbols, such as comma and period.\nHow would it turn out to be?")
+
+	k.Process()
+
+	fmt.Println(k.Values)
 }
