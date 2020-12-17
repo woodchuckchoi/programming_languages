9b9184b172a0f6a92e67dba42310285580a516a7
woodchuckchoi <choiggoggi@gmail.com>
2020-12-17 23:26:03 +0900

    distributed (durability) system features implementation in progress

--- go/assignment/dist/master/master.go
+++ go/assignment/dist/master/master.go
@@ -1,25 +1,63 @@
 package master
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
 	"sort"
 
 	"github.com/google/uuid"
 )
 
 type Address struct {
-	IP   string
-	PORT int
+	IP   string	`json:"ip"`
+	PORT int	`json:"port"`
 }
 
 type Worker struct {
-	UID  string
-	Addr Address
-	Load int
+	UID  string		`json:"uid"`
+	Addr Address	`json:"addr"`
+	Load int		`json:"load"`
 }
 
 type Master struct {
-	Workers      []Worker
-	KeyWorkerMap map[string][]string
+	Workers      []Worker 				`json:"workers,omitempty"`
+	KeyWorkerMap map[string][]string	`json:"key_worker_map,omitempty"`
+}
+
+func (m *Master) loadSavePoint() {
+	savePoints, _ := ioutil.ReadDir("../save/")
+	if len(savePoints) == 0 {
+		return
+	}
+
+	latest := savePoints[0].Name()
+	size := savePoints[0].Size()
+
+	for _, f := range savePoints[1:] {
+		if f.Name() > latest {
+			latest = f.Name()
+			size = f.Size()
+		}
+	}
+
+	buffer := make([]byte, size)
+	
+	f, err := os.Open("../save/"+latest)
+	defer f.Close()
+	
+	if err != nil {
+		return
+	}
+
+	i, _ := f.Read(buffer)
+	master := new(Master)
+
+	err = json.Unmarshal(buffer[:i], master)
+	if err != nil {
+		return
+	}
+	m = *master
 }
 
 func (m *Master) Len() int {
@@ -114,4 +152,13 @@ func getWorkerByUID(m *Master, uid string) *Worker {
 	return ret
 }
 
+func Main(m *Master) {
+	if 
+	for {
+		// logger
+		// healthCheck - all workers
+		// clientRequestHandler - getLeastBusy
+		// workerRequestHandler - acknowledge
+	}
+}
 // modules are pretty much done? Now distributed, cocurrnet service
