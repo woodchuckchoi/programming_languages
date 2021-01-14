c40fd3c4db3a89556cfe61f489c7e03d2e9bf4f4
woodchuckchoi <choiggoggi@gmail.com>
2021-01-14 23:35:51 +0900

    havent thougth about vectors for quite some time, need revision

--- go/graphics/space.go
+++ go/graphics/space.go
@@ -1,5 +1,7 @@
 package graphics
 
+import "math"
+
 /*
    Z
    |
@@ -10,10 +12,39 @@ X
 */
 
 type Point struct {
-	X int
-	Y int
-	Z int
+	X float64
+	Y float64
+	Z float64
 }
+
+type Vector Point
+
+var (
+	DefaultOrigin = Point{0, 0, 0}
+	DefaultView   = Point{1, 1, 1}
+)
+
+func VectorMultiply(v1, v2 Vector) float64 {
+	return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z 
+}
+
+func VectorSize(v Vector) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
+}
+
+func GetAngleBetweenTwoVectors(v1, v2 Vector) float64 {
+	cos := VectorMultiply(v1, v2)/VectorSize(v1)/VectorSize(v2)
+	return math.Acos(cos)
+}
+
+func XLocation(origin, view, pt Point) float64 {
+	viewOnOrigin := Vector{view.X-origin.X, view.Y-origin.Y, 0}
+	ptOnOrigin := Vector{pt.X-origin.X, pt.Y-origin.Y, 0}
+	rad := GetAngleBetweenTwoVectors(viewOnOrigin, ptOnOrigin)
+	return VectorSize(Vector(pt)) * math.Sin(rad)
+}
+
+func Y
 
 func InitPoint(x, y, z int) Point {
 	return Point{
@@ -83,10 +114,4 @@ func Visualise(o *Object) {
 	graphicsWithAxes := addAxes(graphics)
 	graphicsWithPoints := addPoints(graphicsWithAxes, o)
 	printByteSlice(graphicsWithPoints)
-}
-
-type Vector struct {
-	X float64
-	Y float64
-	Z float64
 }
